e196ef1f4e13cf848e3d53a4fa5a5fed3e6b74e5
Lee Folkman <lee.folkman@gmail.com>
2023-02-22 22:34:12 -0500

    Rough draft

---
+++ src/lib/assets/components/TopBar.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import Logo from '$lib/assets/logo.png?w=180&webp'
+  import {
+    Navbar,
+    NavbarBrand,
+    NavbarToggler,
+    Collapse,
+    Nav,
+    NavItem,
+    NavLink,
+    Image
+  } from 'sveltestrap'
+
+  let isOpen = false
+  const toggle = () => isOpen = !isOpen
+</script>
+
+<Navbar>
+  <NavbarBrand>
+    <Image class="me-auto" alt="Butter n' Honey logo" src={Logo} />
+  </NavbarBrand>
+  <NavbarToggler class="me-2" on:click={toggle} />
+  <Collapse {isOpen} navbar>
+    <Nav navbar>
+      <NavItem>
+        <NavLink href="#home">Home</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#plan">Get a mapping plan</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#mapping">What is scripture mapping?</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#about">About</NavLink>
+      </NavItem>
+    </Nav>
+  </Collapse>
+</Navbar>
+
+<style>
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,62 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import TopBar from '$lib/assets/components/TopBar.svelte'
+  import { height, src as Main} from '$lib/assets/main.jpg?width=400&metadata'
+  import Mapping from '$lib/assets/mapping.jpg'
+  import About from '$lib/assets/about.jpg'
+  import { Styles, Button, Container, Row, Col, Input, Image } from 'sveltestrap'
+
+  let isOpen = false
+</script>
+
+<Styles />
+<TopBar />
+
+<Container class="px-0">
+  <div class="text-center" style="height: {height}px; background-image: url({Main}); background-size: cover;">
+    <Row>
+      <Col>
+        <h1>Ready to Scripture map?</h1>
+        <Button>Get a Mapping Plan!</Button>
+      </Col>
+    </Row>
+  </div>
+</Container>
+
+<Container>
+  <Row>
+    <Col>
+      <Input placeholder="First name" />
+      <Input placeholder="Last name" />
+      <Input placeholder="Email" />
+      <Button>Send Me the Plan!</Button>
+    </Col>
+  </Row>
+
+  <Row>
+    <Col>
+      <Image fluid alt="Open scripture mapping journal with a mapped scripture" src={Mapping} />
+      <h1>What is Scripture Mapping?</h1>
+      <p>
+        It's a visual Bible study method to dig deeper into God's word. Map a verse, a word, a Bible
+        character, a topic, an object, and many more!
+      </p>
+    </Col>
+  </Row>
+
+  <Row>
+    <Col>
+      <h1>About</h1>
+      <p>
+        "Butter and honey shall he eat, that he may know to refuse the evil, and choose the good."
+        -- Isa. 7:15
+      </p>
+      <p>
+        Butter n' Honey is an Etsy shop that provides Bible study tools, such as Scripture mapping
+        journals, to help others to discern between good & evil.
+      </p>
+      <Image fluid alt="Four scripture mapping journals spread out" src={About} />
+
+      <Button>Shop Here</Button>
+    </Col>
+  </Row>
+</Container>
