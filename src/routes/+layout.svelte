89496d5e6b4786d05bf06d665ffda199739e598f
Lee Folkman <lee.folkman@gmail.com>
2023-04-09 22:44:28 -0400

    Try to get prerender to work

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+import '/src/global.scss'
+import TopBar from '$lib/components/TopBar.svelte'
+import Banner from '$lib/components/Banner.svelte'
+import PlanForm from '$lib/components/PlanForm.svelte'
+import Mapping from '$lib/components/Mapping.svelte'
+import About from '$lib/components/About.svelte'
+import Footer from '$lib/components/Footer.svelte'
+import { Container, Alert } from 'sveltestrap'
+import { showNotification } from '$lib/store'
+</script>
+
+<TopBar />
+<Container>
+  <Alert color="success" isOpen={$showNotification} dismissible>
+    Yay! You will receive an email with download details. Check your spam folder if you don't find
+    it in your inbox.
+  </Alert>
+  <Banner />
+  <div class="spacer" />
+  <PlanForm />
+  <div class="spacer" />
+  <Mapping />
+  <div class="spacer" />
+  <About />
+  <div class="spacer" />
+  <Footer />
+  <div class="spacer" />
+</Container>
+<slot />
+
+<style lang="scss">
+.spacer {
+  height: 60px;
+}
+</style>
--- src/routes/[...path]/+page.svelte
+++ src/routes/[...path]/+page.svelte
@@ -1,42 +1,8 @@
 <script lang="ts">
-  import '/src/global.scss'
-  import TopBar from '$lib/components/TopBar.svelte'
-  import Banner from '$lib/components/Banner.svelte'
-  import PlanForm from '$lib/components/PlanForm.svelte'
-  import Mapping from '$lib/components/Mapping.svelte'
-  import About from '$lib/components/About.svelte'
-  import Footer from '$lib/components/Footer.svelte'
-  import { Container, Alert } from 'sveltestrap'
-  import type { PageData } from './$types'
+import type { PageData } from './$types'
+import { showNotification } from '$lib/store'
 
-  export let data: PageData
-  let showNotification = false
+export let data: PageData
 
-  $: if (data.path === 'thanks') {
-    showNotification = true
-  }
+$: if (data.path === 'thanks') showNotification.set(true)
 </script>
-
-<TopBar />
-<Container>
-  <Alert color="success" isOpen={showNotification} dismissible>
-    Yay! You will receive an email with download details. Check your spam folder if you don't find
-    it in your inbox.
-  </Alert>
-  <Banner />
-  <div class="spacer" />
-  <PlanForm />
-  <div class="spacer" />
-  <Mapping />
-  <div class="spacer" />
-  <About />
-  <div class="spacer" />
-  <Footer />
-  <div class="spacer" />
-</Container>
-
-<style lang="scss">
-.spacer {
-  height: 60px;
-}
-</style>
